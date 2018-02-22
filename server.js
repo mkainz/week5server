176762afd74fc16fe09cf2139e03932a82566538
studentuser <uceskai@ucl.ac.uk>
2018-02-22 10:55:22 -0500

    A Javascript file to test AJAX

--- server.js
+++ server.js
@@ -4,4 +4,10 @@ var app = express();
 // serve static files - e.g. html, css
 app.use(express.static(__dirname));
 
-
+var https = require('https');
+var fs = require('fs');
+var privateKey = fs.readFileSync('/home/studentuser/certs/client-key.pem').toString();
+var certificate = fs.readFileSync('/home/studentuser/certs/client-cert.pem').toString();
+var credentials = {key: privateKey, cert: certificate};
+var httpsServer = https.createServer(credentials, app);
+httpsServer.listen(4443);
