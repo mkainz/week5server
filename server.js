c73902ecdee0dde2e3226ea2614ed6dc815de000
studentuser <uceskai@ucl.ac.uk>
2018-02-22 09:37:14 -0500

    A new secure server linked to the test website

--- server.js
+++ server.js
@@ -14,5 +14,6 @@ app.get('/test.html', function (req, res) {
  // run some server-side code
 console.log('test.html requested');
 // note that __dirname gives the path to the server.js file
-res.sendFile(__dirname + '/test.html');
+res.sendFile('/home/studentuser/code/week5server' + '/test.html');
 });
+
