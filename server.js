e4e6bb82a27ba4a2d3ca49d9b58e2ad12c51c12f
studentuser <uceskai@ucl.ac.uk>
2018-02-22 09:31:31 -0500

    A new secure server linked to the test website

--- server.js
+++ server.js
@@ -10,8 +10,9 @@ var credentials = {key: privateKey, cert: certificate};
 var httpsServer = https.createServer(credentials, app);
 httpsServer.listen(4443);
 
-app.get('/', function (req, res) {
-	 // run some server-side code
-			console.log('the server has received a request');
-			res.send('Hello World');
+app.get('/test.html', function (req, res) {
+	// run some server/side code
+		console.log('test.html requester');
+		// note that __dirname gives the path to the server.js file
+		res.sendFile(__dirname + '/test.html');
 });
